28308efa873879cc3b9907f97f2d3baaa5916172
arisris <aris@mail.kliksob.com>
2022-10-31 22:14:06 +0700

    Update

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -1,7 +1,15 @@
+import { useState } from "react";
+
 export default function App() {
+  const [counter, setcounter] = useState(0);
   return (
     <div>
-      <div>Hello World</div>
+      <h3>Current counter: {counter}</h3>
+      <div>
+        <button type="button" onClick={() => setcounter(counter + 1)}>
+          Increment
+        </button>
+      </div>
     </div>
-  )
+  );
 }
